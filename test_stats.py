060b6c6f470ed1fa311768c2fba26fc7d14ea316
Sisi Li <sli37@wisc.edu>
2015-08-27 14:28:28 -0500

    first final version

--- test_stats.py
+++ test_stats.py
@@ -30,10 +30,10 @@ def test_std3():
 
 def test_std4():
 	obs = std([1.0,3.0])
-	exp = 2.0
+	exp = 1.0
 	assert_equal(obs,exp)
 
-def test_stde():
+def test_std5():
         obs = std([1.0,1.0,1.0])
         exp = 0.0
         assert_equal(obs,exp)
