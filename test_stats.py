0f7f56db9a0f78108123345a8e47cb71fbc9ef9f
Sisi Li <sli37@wisc.edu>
2015-08-27 13:46:18 -0500

    fixed things

--- test_stats.py
+++ test_stats.py
@@ -2,13 +2,13 @@ from stats import mean
 from nose.tools import assert_equal
 
 def test_mean():
-        assert(mean([2,4]), 3)
+        assert_equal(mean([2,4]) , 3)
 #test_mean()
 
 def test_float_mean():
-        assert(mean([1,2]) == 1.5)
+        assert_equal(mean([1,2]) , 1.5)
 #test_float_mean()
 
 def test_negative_number():
-	assert(mean([-2,-4]) < 0)
+	assert_equal(mean([-2,2,4]) , 1.333)
 #test_negative_number()
