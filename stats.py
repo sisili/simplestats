db613d641f8e32c0cb744f3bd88db3730afea2e6
Sisi Li <sli37@wisc.edu>
2015-08-27 14:25:50 -0500

    added std changes

--- stats.py
+++ stats.py
@@ -4,9 +4,14 @@ def mean(vals):
 	return total/length
 
 def std(vals):
-	if len(vals) == 0:
+	n = len(vals)
+	if n == 0:
 		return 0.0
-	return vals[-1] / 2.0
+	mu = sum(vals)/n
+	var = 0.0
+	for val in vals:
+		var = var + (val-mu)**2
+	return (var/n)**0.5
 
 #print mean([2,4])
 
--- test_stats.py
+++ test_stats.py
@@ -27,3 +27,13 @@ def test_std3():
         obs = std([0.0,4.0])
         exp = 2.0
         assert_equal(obs,exp)
+
+def test_std4():
+	obs = std([1.0,3.0])
+	exp = 2.0
+	assert_equal(obs,exp)
+
+def test_stde():
+        obs = std([1.0,1.0,1.0])
+        exp = 0.0
+        assert_equal(obs,exp)
